c899b89638290b1e370a7b857ea6d912f62d3e1b
Stephen <thesteveo64@gmail.com>
2023-10-11 21:34:56 -0500

    sloppily adding the happy birthday sign into things

--- wordclock/wordclock.h
+++ wordclock/wordclock.h
@@ -6,7 +6,16 @@
 
 // esphome dependencies:
 // needs: esphome time --> id: current_time
-
+int thinking[][2] = {
+    {112, 21}, // T: 12
+    {109, 24}, // H: 5
+    {2, 36}, // I: 9
+    {92, 53}, // N: 8
+    {10, 119}, // K: 2
+    {46, 95}, // I: 9
+    {11, 32}, // N: 8
+    {110, 49}  // G: 2
+};
 int leds_time_it_is[] = {111,112,114,115}; // It Is
 
 int leds_time_minutes[][15] = {
@@ -39,11 +48,21 @@ int leds_time_hours[][6] = {
     { 27,  28,  29,  30,  31,  32}  // eleven
 };
 
+int leds_happy_birthday[][8] = {
+    {109, 88, 87, 66, 65, -1, -1, -1}, // happy
+    {120, 99, 98, 77, 76, 55, 54, 33} // birthday
+};
+
 int hour = -1;
 int minute = -1;
 int red = 124;
 int green = 124;
 int blue = 124;
+
+int bdred = 0;
+int bdgreen = 0;
+int bdblue = 0;
+
 int brightness = 50; // half brightness
 int prevLightReading = 0;
 
@@ -77,6 +96,7 @@ class Wordclock : public Component, public CustomAPIDevice {
             for(int i = 0; i < NUM_LEDS; i++) { setPixelColor(i, 0, 0, blue, brightness); show(); delay(10); }
             for(int i = 0; i < NUM_LEDS; i++) { setPixelColor(i, 0, 0, 0, brightness); }
             show();
+            randomSeed(analogRead(0));
             register_service(&Wordclock::on_setled, "setled", {"number","red", "blue", "green"});
             Serial.println("WordClock Setup Complete!");
         }
@@ -96,36 +116,85 @@ class Wordclock : public Component, public CustomAPIDevice {
             int h = time.hour;
             int m = time.minute;
             bool isChanged = false;
+            bool birthday_changed = false;
             auto fastledlight2 = id(clockface).current_values;
-            //convert float 0.0 till 1.0 into int 0 till 255
-            int newRed = (int)(fastledlight2.get_red()*255);
-            int newGreen = (int)(fastledlight2.get_green()*255);
-            int newBlue = (int)(fastledlight2.get_blue()*255);
-            int newBrightness = (int)(fastledlight2.get_brightness()*255);
-            if (red != newRed || blue != newBlue || green != newGreen || brightness != newBrightness) {
-                red = newRed;
-                blue = newBlue;
-                green = newGreen;
-                brightness = newBrightness;
-                isChanged = true;
-            }
+            auto happy_birthday = id(happybirthday).current_values;
+
+
+            
 
             // Brightness set by the light sensor
+            int brightness = (int)(fastledlight2.get_brightness()*255);
             long value = (long) (id(illuminance).sample() * 500);
             int scaledBrightness = map(value, 0, 500, 10, brightness);
             
-            if (value != prevLightReading) {
+            if (10 < abs(scaledBrightness - prevLightReading) ) {
+                isChanged = true;
+                prevLightReading = scaledBrightness;
+            }
+
+int bdBrightness = happy_birthday.get_brightness();
+            int newRed = (int)(happy_birthday.get_red()*255);
+            int newGreen = (int)(happy_birthday.get_green()*255);
+            int newBlue = (int)(happy_birthday.get_blue()*255);
+            if (bdred != newRed || bdblue != newBlue || bdgreen != newGreen) {
+                bdred = newRed;
+                bdblue = newBlue;
+                bdgreen = newGreen;
+                // brightness = newBrightness;
+                birthday_changed = true;
+            }
+
+            if (isChanged || birthday_changed)
+            {
+            }
+
+
+
+            //convert float 0.0 till 1.0 into int 0 till 255
+            newRed = (int)(fastledlight2.get_red()*255);
+            newGreen = (int)(fastledlight2.get_green()*255);
+            newBlue = (int)(fastledlight2.get_blue()*255);
+            if (red != newRed || blue != newBlue || green != newGreen) {
+                red = newRed;
+                blue = newBlue;
+                green = newGreen;
+                // brightness = newBrightness;
                 isChanged = true;
             }
+
+
 
             //check if valid time. Blink red,green,blue until valid time is present
             if (time.is_valid() == false) {
                 ESP_LOGE("loop", "Got invalid time from current_time Time: %i:%i", h, m );
-                setPixelColor(0, 255, 0, 0, scaledBrightness); show(); delay(250);
-                setPixelColor(0, 0, 255, 0, scaledBrightness); show(); delay(250);
-                setPixelColor(0, 0, 0, 255, scaledBrightness); show(); delay(250);
-                setPixelColor(0, 0, 0, 0, scaledBrightness);   show();
+                for (int i = 0; i < 8; i++) {
+                    int random_number = random(1023);
+                    int rRed = random(255);
+                    int rGreen = random(255);
+                    int rBlue = random(255);
+                    if (random_number %2 == 1) {
+                        setPixelColor(thinking[i][0], rRed, rGreen, rBlue, scaledBrightness); show();
+                        delay(250);
+                        setPixelColor(thinking[i][0], 0, 0, 0, scaledBrightness);   show();
+                    } else {
+                        setPixelColor(thinking[i][1], rRed, rGreen, rBlue, scaledBrightness); show();
+                        delay(250);
+                        setPixelColor(thinking[i][1], 0, 0, 0, scaledBrightness);   show();
+                    }
+                }
+                // setPixelColor(0, 255, 0, 0, scaledBrightness); show(); delay(250);
+                // setPixelColor(0, 0, 255, 0, scaledBrightness); show(); delay(250);
+                // setPixelColor(0, 0, 0, 255, scaledBrightness); show(); delay(250);
+                
+                // cycle through the letters in "thinking"
+
             }
+            // happy tit day
+            // twenty four seven c ock
+            // flashing between c ock and oclock
+            // it is c ock
+            //
             else {
                 // only update once in a Minute
                 if(h != hour || m != minute || isChanged) {
@@ -134,18 +203,28 @@ class Wordclock : public Component, public CustomAPIDevice {
                     if (hour >= 0 && time.is_valid() == true){
                         int tmp_hour = hour;
                         int tmp_minute = (minute - (minute % 5));
-                        if(tmp_minute >= 25) { tmp_hour += 1; }
+                        if(tmp_minute >= 35) { tmp_hour += 1; }
                         tmp_minute = tmp_minute / 5;
                         tmp_hour = tmp_hour % 12;
                         int minutessum = minute % 5;
                         // Reset all LED
-                        for(int i = 0; i < NUM_LEDS; i++) {     setPixelColor(i, 0, 0, 0, scaledBrightness); }
-                        for(int i = 0; i < 4; i++) {            setPixelColor(leds_time_it_is[i], red, green, blue, scaledBrightness); }
-                        for(int i = 0; i < 15; i++) {           if(leds_time_minutes[tmp_minute][i] >= 0) { setPixelColor(leds_time_minutes[tmp_minute][i], red, green, blue, scaledBrightness); } }
-                        for(int i = 0; i < 6; i++) {            if(leds_time_hours[tmp_hour][i] >= 0) { setPixelColor(leds_time_hours[tmp_hour][i], red, green, blue, scaledBrightness); } }
+                        for(int i = 0; i < NUM_LEDS; i++) {  setPixelColor(i, 0, 0, 0, scaledBrightness); }
+
+if (bdBrightness > 0) {
+                for (int j = 0; j < 2; j++)
+                {
+                    for(int i = 0; i < 8; i++) {
+                        if(leds_happy_birthday[j][i] >= 0) { setPixelColor(leds_happy_birthday[j][i], bdred, bdgreen, bdblue, scaledBrightness); }
+                    }
+                }
+}
+
+                        for(int i = 0; i < 4; i++) {         setPixelColor(leds_time_it_is[i], red, green, blue, scaledBrightness); }
+                        for(int i = 0; i < 15; i++) {        if(leds_time_minutes[tmp_minute][i] >= 0) { setPixelColor(leds_time_minutes[tmp_minute][i], red, green, blue, scaledBrightness); } }
+                        for(int i = 0; i < 6; i++) {         if(leds_time_hours[tmp_hour][i] >= 0) { setPixelColor(leds_time_hours[tmp_hour][i], red, green, blue, scaledBrightness); } }
                         show();
-                        ESP_LOGD("loop", "Update Time: %i:%i  Brightness: %i RGB: %i-%i-%i", hour, minute, scaledBrightness, red, green, blue);
-                        ESP_LOGD("loop", "Using tmp_hour: %i tmp_minute: %i minutessum: %i", tmp_hour, tmp_minute, minutessum);
+                        // ESP_LOGD("loop", "Update Time: %i:%i  Brightness: %i RGB: %i-%i-%i", hour, minute, scaledBrightness, red, green, blue);
+                        // ESP_LOGD("loop", "Using tmp_hour: %i tmp_minute: %i minutessum: %i", tmp_hour, tmp_minute, minutessum);
                     }
                 }
             }
