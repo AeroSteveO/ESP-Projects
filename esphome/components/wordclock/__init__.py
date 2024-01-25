c8ca41001e7ba5658970a8ac191b9cb3f2cfe167
stephen <thesteveo64@gmail.com>
2024-01-24 19:03:29 -0600

    unmessing things up hopefully

--- esphome/components/wordclock/__init__.py
+++ esphome/components/wordclock/__init__.py
@@ -16,11 +16,11 @@ CONFIG_SCHEMA = (
 ).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield light.register_light(var, config)
+    var = cg.new_Pvariable(config[CONF_ID])    
+    yield cg.register_component(var, config)   
     
     if CONF_LIGHT in config:
-        light = yield cg.register_component(var, config)
+        light = yield light.register_light(var, config)
         cg.add(var.set_clock_face_lights(light))
     
 #    if CONF_TEMPERATURE in config:
