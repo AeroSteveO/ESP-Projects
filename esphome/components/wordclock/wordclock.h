2f576b8bd2703abcb8f3a91e43b655a679055c5c
stephen <thesteveo64@gmail.com>
2024-01-30 14:43:52 -0600

    implementing apis

--- esphome/components/wordclock/wordclock.h
+++ esphome/components/wordclock/wordclock.h
@@ -31,6 +31,13 @@ class WordClock : public light::AddressableLight, public Component {
 		void add_leds(uint8 num_leds, uint8 data_pin) { pixels = new Adafruit_NeoPixel(num_leds, data_pin, NEO_GRB + NEO_KHZ800); }
 		void set_clock_face_lights(light::AddressableLight *inputClockFace) { clock_face = inputClockFace; }
 		void set_time_id(time::RealTimeClock *time_id) { this->time_id_ = time_id; }
+		
+		
+		  void clear_effect_data() override {
+//			for (int i = 0; i < this->size(); i++)
+//			  this->effect_data_[i] = 0;
+		  }
+		  int32_t size() const override { return this->pixels->numPixels(); }
 	protected:
 		void setPixelColor( uint16_t i, uint8_t r, uint8_t g, uint8_t b, uint16_t uiBrightness);
 		void show();
