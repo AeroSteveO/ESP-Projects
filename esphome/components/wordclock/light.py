b51b37b6542e86c3629336b2e2b98bc94726c04e
stephen <thesteveo64@gmail.com>
2024-01-31 13:20:35 -0600

    try 2

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -13,7 +13,7 @@ CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend({
     cv.Required(CONF_PIN): pins.internal_gpio_output_pin_number, 
     cv.Required(CONF_NUM_LEDS): cv.positive_not_null_int,
 #    cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput)
-})
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
